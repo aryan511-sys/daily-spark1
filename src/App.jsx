0e3fca5f4a40bfb1e41482c35feeaad5a1a7ac9d
aryan511-sys <gajerarajesh4317@gmail.com>
2024-08-12 18:17:32 +0530

    Add some changez

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,127 @@
-import React, { Component } from 'react'
-import NavBar from './Components/NavBar'
-import News from './Components/News'
-import './App.css'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import NavBar from "./Components/NavBar";
+import News from "./Components/News";
+import "./App.css";
+import PropTypes from "prop-types";
+import AboutUs from "./Components/AboutUs";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 export default class App extends Component {
-  
   static defaultProps = {
-    pageSize : 6,
-    country : "us",
-    category : "sports"
-  }
+    pageSize: 6,
+    country: "in",
+    category: "sports",
+  };
 
   static propTypes = {
-    pageSize : PropTypes.number,
-    country : PropTypes.string,
-    category : PropTypes.string,
+    pageSize: PropTypes.number,
+    country: PropTypes.string,
+    category: PropTypes.string,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDarkMode: false,
+    };
   }
 
+  toggleDarkMode = () => {
+    this.setState((prevState) => ({
+      isDarkMode: !prevState.isDarkMode,
+    }));
+  };
   render() {
+    const { isDarkMode } = this.state;
+    const themeClass = isDarkMode ? "dark-mode" : "light-mode";
     return (
       <div>
-        <NavBar />
-        <News pageSize = {6} country={"us"} category={"sports"} />
+        <Router>
+          <div className={`app ${themeClass}`}>
+            <NavBar toggleMode={this.toggleDarkMode} />
+
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <News
+                    key="general"
+                    pageSize={6}
+                    country={"in"}
+                    category={"general"}
+                  />
+                }
+              ></Route>
+              <Route
+                path="/business"
+                element={
+                  <News
+                    key="business"
+                    pageSize={6}
+                    country={"in"}
+                    category={"business"}
+                  />
+                }
+              ></Route>
+              <Route
+                path="/entertainment"
+                element={
+                  <News
+                    key="entertainment"
+                    pageSize={6}
+                    country={"in"}
+                    category={"entertainment"}
+                  />
+                }
+              ></Route>
+              <Route
+                path="/health"
+                element={
+                  <News
+                    key="helth"
+                    pageSize={6}
+                    country={"in"}
+                    category={"health"}
+                  />
+                }
+              ></Route>
+              <Route
+                path="/science"
+                element={
+                  <News
+                    key="science"
+                    pageSize={6}
+                    country={"in"}
+                    category={"science"}
+                  />
+                }
+              ></Route>
+              <Route
+                path="/sports"
+                element={
+                  <News
+                    key="sports"
+                    pageSize={6}
+                    country={"in"}
+                    category={"sports"}
+                  />
+                }
+              ></Route>
+              <Route
+                path="/technology"
+                element={
+                  <News
+                    key="technoligy"
+                    pageSize={6}
+                    country={"in"}
+                    category={"technology"}
+                  />
+                }
+              ></Route>
+              <Route path="/comingsoon" element={<AboutUs />}></Route>
+            </Routes>
+          </div>
+        </Router>
       </div>
-    )
+    );
   }
 }
